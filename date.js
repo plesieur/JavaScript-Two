303f91af5c2d59771cbfca64ea4c2470a599e31f
Paul <71508910+plesieur@users.noreply.github.com>
2024-05-20 07:24:02 -0400

    Add files via upload
    Original

---
+++ date.js
@@ -0,0 +1,10 @@
+function getDate() 
+{
+	var dateString = new Date();
+	document.getElementById("date").innerHTML=dateString;
+}
+
+function purple()
+{
+	document.getElementById("date").style.color="purple";
+}
